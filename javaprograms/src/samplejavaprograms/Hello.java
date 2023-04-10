3ff9f040278f4950ab55368f95ebe941284d89c9
pravin.kr <pravin.kr@PIC4437.pitsolutions.com>
2023-04-10 10:40:06 +0530

    First Commit

---
+++ javaprograms/src/samplejavaprograms/Hello.java
@@ -0,0 +1,17 @@
+package samplejavaprograms;
+
+public class Hello {
+	
+	public void input()
+	{
+		System.out.println("Hello World");
+		
+	}
+
+	public static void main(String[] args) {
+		Hello obj=new Hello();
+		obj.input();
+		
+	}
+
+}
